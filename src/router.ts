3a717836db8c4fce2fcc9c40bc975cef8140d3c9
Azee <aziztitu1996@gmail.com>
2019-02-11 00:30:30 -0600

    Removes the dashboard path.

--- src/router.ts
+++ src/router.ts
@@ -19,10 +19,6 @@ export default new Router({
     routes: [
         {
             path: '/',
-            redirect: '/dashboard',
-        },
-        {
-            path: '/dashboard',
             component: Dashboard,
             children: [
                 {
